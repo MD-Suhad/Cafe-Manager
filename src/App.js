0090a84882dc0f92456805c34db815f660c0a2b4
MD-Suhad <ssuhad490@gmail.com>
2024-01-29 22:26:41 +0600

    personal project updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
        <h1>
         cafe manager code start consistency in real life
+        <h2>this is industrial project in the part of life</h2>
         </h1>
        
       

570754482bddea26818b6f32791d8ee5fe522dc1
MD-Suhad <ssuhad490@gmail.com>
2024-02-21 23:16:33 +0600

    project updated

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,6 +5,7 @@ const Navbar = () => {
       <div>
          <h1>this is navbar in the website</h1>
          <h1>project updated in the website</h1>
+         <h3>this is product in simple project </h3>
       </div>
    );
 };

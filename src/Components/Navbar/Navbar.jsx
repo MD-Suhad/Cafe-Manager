cc87aab4257c2255ffe93de36c48d1337bb95025
MD-Suhad <ssuhad490@gmail.com>
2024-02-29 21:47:26 +0600

    point of sale porject start

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 const Navbar = () => {
    return (
       <div>
-         <h1>this is navbar in the website</h1>
-         <h1>project updated in the website</h1>
-         <h3>this is product in simple project </h3>
+         <h1>Point of Sale Project Start & Today</h1>
       </div>
    );
 };

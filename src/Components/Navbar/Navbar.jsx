95b688d472c731efe659d99ffe9b23fd5f06b2d2
MD-Suhad <ssuhad490@gmail.com>
2024-03-01 23:36:00 +0600

    project updated in design

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import "./Navbar.css";
 
 const Navbar = () => {
    return (
       <div>
-         <h1>Point of Sale Project Start & Today</h1>
+         <nav className="navbar">
+      <div className="navbar-brand">
+        <span className="navbar-item">POS System</span>
+      </div>
+      <div className="navbar-menu">
+        <div className="navbar-end">
+          <a href="/" className="navbar-item">Home</a>
+          <a href="/products" className="navbar-item">Products</a>
+          <a href="/cart" className="navbar-item">Cart</a>
+          <a href="/checkout" className="navbar-item">Checkout</a>
+        </div>
+      </div>
+    </nav>
       </div>
    );
 };

9f4ab47aff271373a3b1b5a9a67d67820476a97b
MD-Suhad <ssuhad490@gmail.com>
2024-03-13 23:29:51 +0600

    project updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-// import logo from './logo.svg';
-// import './App.css';
-// import Navbar from './Components/Navbar/Navbar';
 
-// function App() {
-//   return (
-//     <div className="App">
-//        <h1>
-//         cafe manager code start consistency in real life
-//         <h2>this is industrial project in the part of life</h2>
-//         </h1>
-//        <Navbar/>
-      
-//     </div>
-//   );
-// }
-
-// export default App;
-// App.js
 import React, { useState } from 'react';
 import ProductList from './Components/Navbar/ProductList';
 import Cart from './Components/Navbar/Cart';
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+   return (
+      <div>
+         <h3>this is project right click the home page</h3>
+      </div>
+   );
+};
+
+export default Home;

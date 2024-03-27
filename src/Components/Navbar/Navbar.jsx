f1e9c97fa740e5990c611341a531412d3bdb5b03
MD-Suhad <ssuhad490@gmail.com>
2024-03-27 23:34:24 +0600

    project prblem added

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,16 +1,32 @@
-import React from 'react';
+import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-   return (
+  return (
+    <div>
       <div>
-  
-      <div className="navbar-brand">
-        <span className="navbar-item">Z.A Pharma</span>
-        <p>Biswas Tower market, Kansat GopalNagar Mor Shibgong,Chapalnawabganj,Bangladesh</p>
+        <div className="navbar-brand">
+          <span className="navbar-item">Z.A Pharma</span>
+          <p>
+            Biswas Tower market, Kansat GopalNagar Mor
+            Shibgong,Chapalnawabganj,Bangladesh
+          </p>
+        </div>
+        <div className="navbar-right">
+          <h3>Username :</h3>
+          <h3>Designation :</h3>
+          <h3>Department :</h3>
+          <h4>Logged on at : Logged on at: web Mar 06,2024 01:15pm</h4>
+        </div>
+        <div className="navbar-bottom">
+          <h3>
+            This ERP is Developed and Maintained By Lumineux Informatique
+            Bangladesh
+          </h3>
+        </div>
       </div>
-      </div>
-   );
+    </div>
+  );
 };
 
 export default Navbar;

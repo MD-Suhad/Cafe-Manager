3b51787acbbea079aa944e539e24df7d5b29d176
MD-Suhad <ssuhad490@gmail.com>
2024-04-15 23:20:59 +0600

    project start

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,35 +1,11 @@
 import React from 'react';
-import './Home';
-import Login from '../Login/Login';
 
 const Home = () => {
    return (
-      
-    <Login> </Login>
+      <div>
+         <h2>this is home page</h2>
+      </div>
    );
 };
 
 export default Home;
-
-// export default Home;
-// import {
-//    createBrowserRouter,
-//    createRoutesFromElements,
-//    Route,
-//    RouterProvider,
-//  } from "react-router-dom";
- 
-//  const router = createBrowserRouter(
-//    createRoutesFromElements(
-//      <Route path="/" element={<Root />}>
-//        <Route path="dashboard" element={<Dashboard />} />
-//        {/* ... etc. */}
-//      </Route>
-//    )
-//  );
- 
-//  ReactDOM.createRoot(document.getElementById("root")).render(
-//    <React.StrictMode>
-//      <RouterProvider router={router} />
-//    </React.StrictMode>
-//  );

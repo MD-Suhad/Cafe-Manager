76f00e06f4373c2a32604420f0fc2775c3df5c18
MD-Suhad <ssuhad490@gmail.com>
2024-04-21 22:47:00 +0600

    project navbar undated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,59 +1,48 @@
-import React, { useState } from 'react';
-import './Home.css'
+import React from 'react';
 
 const Home = () => {
-   // adding the states 
-  const [isActive, setIsActive] = useState(false);
-  //add the active class
-  const toggleActiveClass = () => {
-    setIsActive(!isActive);
-  };
-  //clean up function to remove the active class
-  const removeActive = () => {
-    setIsActive(false)
-  }
-   return (
-      <div className="App">
-      <header className="App-header">
-        <nav //className={`${styles.navbar}`}
-        >
-          {/* logo */}
-          <a href='#home' //className={`${styles.logo}`}
-          >Dev. </a>
-          <ul //className={`${styles.navMenu} ${isActive ? styles.active : ''}`}
-          >
-            <li onClick={removeActive}>
-              <a href='#home' //className={`${styles.navLink}`}
-              >Home</a>
-            </li>
-            <li onClick={removeActive}>
-              <a href='#home' //className={`${styles.navLink}`}
-              >Catalog</a>
-            </li>
-            <li onClick={removeActive}>
-              <a href='#home' //className={`${styles.navLink}`}
-              >All products</a>
-            </li>
-            <li onClick={removeActive}>
-              <a href='#home' //className={`${styles.navLink}`}
-              >Contact</a>
-            </li>
+  return (
+    
+    
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
           </ul>
-          <div //className={`${styles.hamburger} ${isActive ? styles.active : ''}`}  
-          onClick={toggleActiveClass}>
-            <span //className={`${styles.bar}`}
-            ></span>
-            <span //className={`${styles.bar}`}
-            ></span>
-            <span //className={`${styles.bar}`}
-            >
-
-            </span>
-          </div>
-        </nav>
-      </header>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
     </div>
-   );
+  </div>
+</nav>
+
+
+  );
 };
 
 export default Home;

4b7ff645ffd474f7bcd379962e72c6c9bb30b9a1
MD-Suhad <ssuhad490@gmail.com>
2024-05-14 23:52:59 +0600

    project updated

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import React, { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Example from './Components/Navigation/navigation';
+import Login from './Components/Login/Login';
 
 const App = () => {
 
 
   return (
     <div className="App">
-      {/* <Home/> */}
+     
       <Example/>
       <Navbar/>
+      <Login/>
     
       
     </div>

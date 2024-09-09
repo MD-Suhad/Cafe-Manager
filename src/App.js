50e2104bc9ca59d3dbe3d0cd0ed8f76beb255b64
MD-Suhad <ssuhad490@gmail.com>
2024-09-09 23:18:55 +0600

    bug fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Sidebar from './Components/Sidebar/Sidebarr';
 import Login from './Components/Login/Login';
-import { BrowserRouter } from 'react-router-dom';
 import { Link } from "react-router-dom";
 import Dashboard from './Components/Dashboard/Dashboard';
 
@@ -21,7 +20,7 @@ const App = () => {
       <Sidebar/>
       <Dashboard/>
       <Login/>
-      {/* <Link to = {Login} >{Login}</Link> */}
+       <Link to = {Login} >{Login}</Link> 
 
 
       
